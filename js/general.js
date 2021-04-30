ca9a1c80bce6873bd4458984b3226419eb51c561
manalsolanki <97manal@gmail.com>
2021-04-30 11:33:45 -0400

    Final CHnages Done.

--- js/general.js
+++ js/general.js
@@ -1,10 +1,10 @@
-window.onload = function (){
-    var subscriptionForm = document.subscribeBtn ;
-    subscriptionForm.onclick = thnksMsg;
-    function thnksMsg()
-    {
+// window.onload = function (){
+//     var subscriptionForm = document.subscribeBtn ;
+//     subscriptionForm.onclick = thnksMsg;
+//     function thnksMsg()
+//     {
     
-        console.log("he")
-       return false
-    }
-}
+//         console.log("he")
+//        return false
+//     }
+// }
